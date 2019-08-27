adeffce55618224ef7ac2491be2e677fead2e38a
kimch <kimch@192.168.1.107>
2019-08-27 19:39:09 +0800

    V0.20 单例模式(构造方法私有化)懒汉式

--- src/org/spring/bean/Singleton1.java
+++ src/org/spring/bean/Singleton1.java
@@ -1,15 +1,19 @@
 package org.spring.bean;
 
 class Singleton { // 定义一个类
-    private static final Singleton INSTANCE = new Singleton() ;
+    private static Singleton instance ;
     private Singleton() {}   // 构造方法私有化
     public void print() {
-    	System.out.println("Hello World .") ;
+              System.out.println("Hello World .") ;
     }
     public static Singleton getInstance() {
-    	return INSTANCE ;
+              if (instance == null) {   // 没有实例化
+                       instance = new Singleton() ;  // 实例化
+              }
+              return instance ;
     }
 }
+
 public class Singleton1 {
     public static void main(String args[]) {
         Singleton inst = null ;   // 声明对象
