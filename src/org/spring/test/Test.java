2db41b9a5a8189948eacb0e06281d2ed7e614f34
Kim <Kim@Kim-PC>
2019-08-27 16:16:41 +0800

    V0.5 构造注入

--- src/org/spring/test/Test.java
+++ src/org/spring/test/Test.java
@@ -1,5 +1,6 @@
 package org.spring.test;
 
+import org.spring.bean.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,18 +8,10 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		HelloWorld hw = new HelloWorld();
-		hw.setName("123");
-		hw.hello();
-		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		HelloWorld h = (HelloWorld) ctx.getBean("helloWorld");
-		h.hello();
-		
-		System.out.println("hw:"+hw);
-		System.out.println("h:"+h);
+		Car car = (Car) ctx.getBean("car");
+		System.out.println(car.toString());
 		
 	}
-	
 	
 }
