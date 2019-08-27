773e4ab9628179ba9eb3a2685762638a37fdd72c
kimch <kimch@192.168.1.107>
2019-08-27 19:48:16 +0800

    V0.21 注册 PropertyPlaceholderConfigurer

--- src/org/spring/test/Test.java
+++ src/org/spring/test/Test.java
@@ -1,7 +1,10 @@
 package org.spring.test;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.spring.bean.Car;
-import org.spring.bean.DataSource;
 import org.spring.bean.Person;
 import org.spring.bean.Person2;
 import org.springframework.context.ApplicationContext;
@@ -9,30 +12,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Car car = (Car) ctx.getBean("car");
+		Car car2 = (Car) ctx.getBean("car");
 		System.out.println(car.toString());
-		
-		Car car2 = (Car) ctx.getBean("car2");
 		System.out.println(car2.toString());
-		
-		Person p = (Person) ctx.getBean("person");
-		System.out.println(p.toString());;
-		
-		Person2 p2 = (Person2)ctx.getBean("person5");
-		System.out.println(p2.toString());;
-		
-		DataSource datasource = (DataSource) ctx.getBean("dataSource");
-		System.out.println(datasource);
-		
-		Person2 p3 = (Person2)ctx.getBean("person7");
-		System.out.println(p3.toString());;
-		System.out.println("----------");
-		Person person = (Person) ctx.getBean("person");
-		System.out.println(person);
-		
+		DataSource ds = (DataSource)ctx.getBean("dataSource");
+		System.out.println(ds.getConnection());
+
 	}
 	
 }
