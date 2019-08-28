a7edfed14e020cf53c222f6dfe4770b5c026e5dd
Kim <Kim@Kim-PC>
2019-08-28 17:06:05 +0800

    V0.36 JDBC查询操作2

--- src/com/hp/spring/JDBCTest.java
+++ src/com/hp/spring/JDBCTest.java
@@ -5,9 +5,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.tree.RowMapper;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
 
@@ -45,4 +48,27 @@ public class JDBCTest {
 		
 	}
 
+	
+	@Test
+	public void testQueryForObject() {
+		
+		String sql = "select count(id) from employees";
+		Integer n = jdbcTemplate.queryForObject(sql,Integer.class);
+		
+		System.out.println(n);
+		
+	}
+	
+	@Test
+	public void testQueryForList2() {
+		
+		String sql = "select id,last_name lastName,email from employees where id>?";
+		
+		RowMapper<Employee> rowMapper = new BeanPropertyRowMapper<Employee>(Employee.class);
+		
+		List<Employee> employees = jdbcTemplate.query(sql, rowMapper, 1);
+		
+		System.out.println(employees);
+		
+	}
 }
