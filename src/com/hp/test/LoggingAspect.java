1fa67bcd569076748f4ff4d869b5621714ea7e9b
Kim <Kim@Kim-PC>
2019-08-28 13:36:23 +0800

    V0.33 重用切入点定义

--- src/com/hp/test/LoggingAspect.java
+++ src/com/hp/test/LoggingAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -16,62 +17,24 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-
-	@Before("execution(public int com.hp.test.ArithmeticCalculator.*(int,int))")
+	
+	@Pointcut("execution(public int com.hp.test.ArithmeticCalculator.*(int,int))")
+	public void declareJoinPointExpression() {}
+	
+	//前置通知
+	@Before("declareJoinPointExpression()")
 	public void beforeMethod(JoinPoint joinPoint) {
-		
 		String methodName = joinPoint.getSignature().getName();
 		
 		Object args[] = joinPoint.getArgs();
 		
-		System.out.println("method begin....."+methodName+",args:"+Arrays.asList(args));
+		System.out.println("前置通知---method begin....."+methodName+",args:"+Arrays.asList(args));
 	}
 	
-	@After("execution(* com.hp.test.*.*(..))")
+	//后置通知，在方法执行之后执行的代码，无论该方法是否出现异常
+	@After("declareJoinPointExpression()")
 	public void afterMethod(JoinPoint joinPoint) {
-		
 		String methodName = joinPoint.getSignature().getName();
-		
-		System.out.println("method:"+methodName+" ends.");
-	}
-	
-	@AfterReturning(pointcut="execution(* com.hp.test.*.*(..))",returning="result")
-	public void afterReturning(JoinPoint joinPoint,Object result) {
-		
-		String methodName = joinPoint.getSignature().getName();
-		
-		System.out.println("返回通知---the method:"+methodName+" end with result:"+result);
-	
-	}
-	
-	@AfterThrowing(pointcut="execution(* com.hp.test.*.*(..))",throwing="e")
-	public void afterThrowing(JoinPoint joinPoint,Exception e) {
-		
-		String methodName = joinPoint.getSignature().getName();
-		
-		System.out.println("异常通知---the method:"+methodName+" occurs exception:"+ e);
-		
-	}
-	
-	@Around("execution(* com.hp.test.*.*(..))")
-	public Object aroundMethod(ProceedingJoinPoint pjd) {
-		Object result = null;
-		String methodName = pjd.getSignature().getName();
-		
-		try {
-			System.out.println("*前置通知---method begin....."+methodName+",args:"+Arrays.asList(pjd.getArgs()));
-			
-			result = pjd.proceed();
-			
-			System.out.println("*返回通知---the method:"+methodName+" end with result:"+result);
-		} catch (Throwable e) {
-			
-			System.out.println("*异常通知---the method:"+methodName+" occurs exception:"+ e);
-//			throw new RuntimeException(e);
+		System.out.println("后置通知---method:"+methodName+" ends.");
 		}
-		
-		System.out.println("*后置通知---method:"+methodName+" ends.");
-		
-		return result;
 	}
-}
