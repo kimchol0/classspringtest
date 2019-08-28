394d827945781510075b76e16d4239f80cd57d68
Kim <Kim@Kim-PC>
2019-08-28 11:34:57 +0800

    V0.28 AspectJ后置通知

--- src/com/hp/test/LoggingAspect.java
+++ src/com/hp/test/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.hp.test;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,13 @@ public class LoggingAspect {
 		Object args[] = joinPoint.getArgs();
 		
 		System.out.println("method begin....."+methodName+",args:"+Arrays.asList(args));
+	}
+	
+	@After("execution(* com.hp.test.*.*(..))")
+	public void afterMethod(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+		
+		System.out.println("method:"+methodName+" ends.");
 	}
 	
 }
